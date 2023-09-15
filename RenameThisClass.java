0868d23afa503baa354ac5214971146040a1c826
Eva Bartolome <eva.bartolome@mrjaffesclass.com>
2023-09-15 11:11:07 -0700

    Commit message

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,12 +9,12 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         String string1 = "I am a string";
-        String string2 = string1;
+        String string2 = string1; 
         String string3 = "I am also a string";
-        boolean stringsEqual = string1.equals(string3);
-        System.out.println("string1: " + string1);
-        System.out.println("string2: " + string2);
-        System.out.println("string3: " + string3);
-        System.out.println("string1: " + stringsEqual);
+        boolean stringsEqual = string1.equals(string3); //comparing w/ equals method
+        System.out.println("string1:" + string1);
+        System.out.println("string2:" + string2);
+        System.out.println("string3:" + string3);
+        System.out.println("string1:" + stringsEqual);
     }
 }
